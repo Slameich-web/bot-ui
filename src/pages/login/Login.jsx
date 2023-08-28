64d7cfe182b083a017b0ed4e934433d2b8b3124c
Slameich-web <slameacer@mail.ru>
2023-08-28 13:47:16 +0300

    init

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { $api } from "../../http/index";
+import "../../App.scss";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const [isRedirect, setIsEedirect] = useState(false);
+
+  const isShowPassword = showPassword ? "text" : "password";
+  const navigate = useNavigate();
+
+  const loginRequest = async () => {
+    try {
+      await $api.post("/api/login", {
+        email: email,
+        password: password,
+      });
+      setIsEedirect(true);
+    } catch (e) {
+      setError(e?.response?.data?.message);
+    }
+  };
+  useEffect(() => {
+    if (isRedirect) {
+      return navigate("/home");
+    }
+  }, [isRedirect, navigate]);
+
+  return (
+    <div className="auth_wrapper">
+      <div className="auth_container">
+        <form>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Пароль"
+            type={isShowPassword}
+          />
+        </form>
+        <div>
+          <span>Показать пароль </span>
+          <input
+            type="checkbox"
+            onChange={() => setShowPassword((prev) => !prev)}
+          />
+        </div>
+        <button onClick={loginRequest} className="login_button">
+          Войти
+        </button>
+        <div>{error}</div>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { $api } from "../../http/index";
+import "../../App.scss";
+import { useNavigate } from "react-router-dom";
+
+export const Register = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const [isRedirect, setIsEedirect] = useState(false);
+  const isShowPassword = showPassword ? "text" : "password";
+  const navigate = useNavigate();
+
+  const registerRequest = async () => {
+    try {
+      await $api.post("/api/register", {
+        email: email,
+        password: password,
+        telegram_id: 5421,
+      });
+      setIsEedirect(true);
+    } catch (e) {
+      setError(e?.response?.data?.message);
+    }
+  };
+
+  useEffect(() => {
+    if (isRedirect) {
+      return navigate("/login");
+    }
+  }, [isRedirect, navigate]);
+
+  return (
+    <div className="auth_wrapper">
+      <div className="auth_container">
+        <form>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Пароль"
+            type={isShowPassword}
+          />
+        </form>
+        <div>
+          <span>Показать пароль </span>
+          <input
+            type="checkbox"
+            onChange={() => setShowPassword((prev) => !prev)}
+          />
+        </div>
+        <button onClick={registerRequest} className="login_button">
+          Регистрация
+        </button>
+        <div>{error}</div>
+      </div>
+    </div>
+  );
+};
