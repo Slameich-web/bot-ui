92f78d2a8b1552d1a6a6e291f01eca03938acb9f
Slameich-web <slameacer@mail.ru>
2023-08-28 22:02:37 +0300

    fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { useEffect } from "react";
-import { $api } from "../../http/index";
+import React, { useState, useEffect } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useTransition } from "@react-spring/web";
+import { animated } from "@react-spring/web";
 import { Link } from "react-router-dom";
-import "../../App.scss";
+import { $api } from "../../http/index";
 
 export const Home = () => {
   const getUser = async () => {
@@ -15,16 +17,63 @@ export const Home = () => {
   useEffect(() => {
     getUser();
   });
-
+  const [showNavbar, setShowNavbar] = useState(false);
+  const transition = useTransition(showNavbar, {
+    from: { x: -100, y: 100, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: { x: 100, y: 100, opacity: 0 },
+  });
   return (
     <div className="auth_wrapper">
       <div className="nav_container">
-        <Link className="buttons" to={"/revenue"}>
-          <button>Доход</button>
-        </Link>
-        <Link className="buttons" to={"/checkouts"}>
-          <button>Запросить выплату</button>
-        </Link>
+        <div className="drop_down">
+          <div
+            className="drop_down_button"
+            onClick={() => setShowNavbar((prev) => !prev)}
+          >
+            {showNavbar ? (
+              <CloseIcon fontSize="large" />
+            ) : (
+              <MenuIcon fontSize="large" />
+            )}
+          </div>
+        </div>
+        {transition((style, item) =>
+          item ? (
+            <animated.div style={style} className="buttons">
+              <Link
+                to={"/revenue"}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  textDecoration: "none",
+                  color: "var(--tg-theme-button-text-color)",
+                }}
+              >
+                <animated.button style={style}>Доход</animated.button>
+              </Link>
+              <Link
+                to={"/checkouts"}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  textDecoration: "none",
+                  color: "var(--tg-theme-button-text-color)",
+                }}
+              >
+                <animated.button style={style}>
+                  Запросить выплату
+                </animated.button>
+              </Link>
+            </animated.div>
+          ) : (
+            ""
+          )
+        )}
       </div>
     </div>
   );
