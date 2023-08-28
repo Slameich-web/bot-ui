a51a2f7263b117020ddb78b6b2b0146ab0ca83bc
Slameich-web <slameacer@mail.ru>
2023-08-28 14:07:34 +0300

    fix2

--- src/http/index.js
+++ src/http/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API_URL = "http://127.0.0.1:8000/";
 
 export const $api = axios.create({
-  withCredentials: false,
+  withCredentials: true,
   mode: "no-cors",
   baseURL: API_URL,
 });
