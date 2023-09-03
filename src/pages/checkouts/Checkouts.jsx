d5c057b0c81c745d7181684e95327f1ed954fe02
Slameich-web <slameacer@mail.ru>
2023-09-03 18:11:17 +0300

    test

--- src/pages/checkouts/Checkouts.jsx
+++ src/pages/checkouts/Checkouts.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CheckoutsDropDown from "./components/CheckoutsDropDown";
 import CheckoutsTable from "./components/CheckoutsTable";
+import { useTelegram } from "../../useTelegram";
+
 export const Checkouts = () => {
   const [showNavbar, setShowNavbar] = useState(false);
+  const { tg } = useTelegram();
+  useEffect(() => {
+    tg.MainButton.hide();
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="checkouts_wrapper">
       <CheckoutsDropDown
--- src/pages/revenue/Revenue.jsx
+++ src/pages/revenue/Revenue.jsx
@@ -30,70 +30,7 @@ export const Revenue = () => {
       <div className="revenue_container">
         <div className="available_funds">Доступно к выводу:</div>
         <Table tableName={"За прошедший месяц"} />
-        <div className="table_wrap">
-          <div className="table_title">
-            <span>tableName</span>
-          </div>
-          <table border="2" className="table">
-            <tbody>
-              <tr>
-                <td></td>
-                <td>Площадка 1</td>
-                <td>Площадка 2</td>
-                <td>Площадка 3</td>
-                <td>Площадка 3</td>
-                <td>Площадка 3</td>
-                <td>Площадка 3</td>
-                <td>Площадка 3</td>
-                <td>Площадка 3</td>
-              </tr>
-              <tr>
-                <td>Продукт 2</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-              </tr>
-              <tr>
-                <td>Продукт 3</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-              </tr>
-              <tr>
-                <td>Продукт 3</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-              </tr>
-              <tr>
-                <td>Продукт 3</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-                <td>1800 ₽</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Table tableName={"За всё время"} />
       </div>
     </div>
   );
