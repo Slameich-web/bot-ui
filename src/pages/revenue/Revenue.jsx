72217a183dc2470a306bc165fe3bab554b139a50
Slameich-web <slameacer@mail.ru>
2023-09-03 17:36:49 +0300

    test

--- src/pages/revenue/Revenue.jsx
+++ src/pages/revenue/Revenue.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Modal from "./components/Modal";
 import RevDropDown from "./components/RevDropDown";
 import Table from "./components/Table";
+import { useTelegram } from "../../useTelegram";
+
 export const Revenue = () => {
   const [showNavbar, setShowNavbar] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const { tg } = useTelegram();
 
+  const showModalClick = useCallback(() => {
+    return () => setShowModal(true);
+  }, []);
+  useEffect(() => {
+    tg.MainButton.show();
+    tg.MainButton.setParams({
+      text: "Вывести в рублях",
+    });
+    tg.onEvent("mainButtonClicked", showModalClick);
+    return () => {
+      tg.offEvent("mainButtonClicked", showModalClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="revenue_wrapper">
       {showModal ? <Modal setShowModal={setShowModal} /> : ""}
@@ -14,14 +31,6 @@ export const Revenue = () => {
         <div className="available_funds">Доступно к выводу:</div>
         <Table tableName={"За прошедший месяц"} />
         <Table tableName={"За всё время"} />
-      </div>
-      <div className="check_out_button_container">
-        <button
-          className="check_out_button"
-          onClick={() => setShowModal((prev) => !prev)}
-        >
-          Вывести в рублях
-        </button>
       </div>
     </div>
   );
