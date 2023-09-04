e87fad5bceb5fa887f7681d8546bf3d59461c8d3
Slameich-web <slameacer@mail.ru>
2023-09-04 11:15:20 +0300

    test

--- src/pages/login/components/LoginInputs.jsx
+++ src/pages/login/components/LoginInputs.jsx
@@ -4,18 +4,12 @@ export const LoginInputs = memo(
   ({ setEmail, setPassword, isShowPassword, setShowPassword }) => {
     return (
       <div className="login_input_container">
-        <input
-          className="auth_input"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
         <div class="text-field text-field_floating-2">
           <input
             class="text-field__input"
             type="email"
             id="email"
             name="email"
-            placeholder="alexander@itchief.ru"
           />
           <label class="text-field__label" for="email">
             Email
