45235d73d49a2c8b5fb20187304b6b8f8c46f4cb
Slameich-web <slameacer@mail.ru>
2023-09-04 11:22:29 +0300

    test

--- src/pages/login/components/LoginInputs.jsx
+++ src/pages/login/components/LoginInputs.jsx
@@ -10,18 +10,24 @@ export const LoginInputs = memo(
             type="email"
             id="email"
             name="email"
+            placeholder="email"
           />
           <label class="text-field__label" for="email">
             Email
           </label>
         </div>
-        <div className="input_with_button">
+        <div className="text-field text-field_floating-2 input_with_button">
           <input
+            class="text-field__input"
             label="Пароль"
+            id="password"
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Пароль"
             type={isShowPassword}
           />
+          <label class="text-field__label" for="password">
+            Пароль
+          </label>
           <VisibilityIcon className="show_button" onClick={setShowPassword} />
         </div>
       </div>
