57b51694889b626f33fd4b62ddd2540d68917cf2
Slameich-web <slameacer@mail.ru>
2023-09-04 11:46:11 +0300

    test

--- src/pages/register/components/RegInputs.jsx
+++ src/pages/register/components/RegInputs.jsx
@@ -12,7 +12,7 @@ export const RegInputs = memo(
     setShowPassword,
   }) => {
     return (
-      <div className="reg_input_container">
+      <div className="login_input_container">
         <div className="text-field text-field_floating-2 input_with_button">
           <input
             className="auth_input"
